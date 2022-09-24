a13d9284a228491d23c78e5c55bc79a179e71a6f
jonesbrett <50668107+jonesbrett@users.noreply.github.com>
2022-09-24 11:09:55 +0100

    Add files via upload

---
+++ Block Letters.py
@@ -0,0 +1,10 @@
+# Brett Jones
+# This is weird to me
+print("B", "B", "B", "B", "  ", "J", "J", "J", "J", "J")
+print("B", "    ", "B", "     ", "J")
+print("B", "    ", "B", "     ", "J")
+print("B", "   ", "B", "      ", "J")
+print("B", "B", "B", "B", "      ", "J")
+print("B", "    ", "B", " ", "J", " ", "J")
+print("B", "    ", "B", " ", "J", " ", "J")
+print("B", "B", "B", "B", "    ", "J", "J")
