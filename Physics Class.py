c49d4cef2b63cd21efe8ba3a2f195ac42c47c62a
jonesbrett <jonesbrett@hotmail.com>
2022-10-04 21:07:49 +0100

    .

---
+++ Physics Class.py
@@ -0,0 +1,51 @@
+# Uncomment this when you reach the "Use the Force" section
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
+
+
+# Write your code below:
+def f_to_c(f_temp):
+    c_temp = (f_temp - 32) * 5 / 9
+
+
+f100_in_celsius = f_to_c(100)
+
+
+def c_to_f(c_temp):
+    f_temp = c_temp * (9 / 5) + 32
+
+
+c0_in_fahrenheit = c_to_f(0)
+
+train_force = 0
+
+
+def get_force(mass, acceleration):
+    global train_force
+    train_force = mass * acceleration
+    return train_force
+
+
+get_force(mass=train_mass, acceleration=train_acceleration)
+print(train_force)
+print("The GE train supplies", train_force, "Newtons of force.")
+
+
+def get_energy(mass, c=3 * 10**8):
+    return mass * c**2
+
+
+bomb_energy = get_energy(mass=bomb_mass)
+print("A 1kg bomb supplies", bomb_energy, "Joules.")
+
+
+def get_work(mass, acceleration, distance):
+    return get_force(mass, acceleration) * distance
+
+
+train_work = get_work(
+    mass=train_mass, acceleration=train_acceleration, distance=train_distance
+)
+print("The GE train does", train_work, "Joules of work over", train_distance, "meters.")
